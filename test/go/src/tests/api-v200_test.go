86c3d7cf4424ea15b725cf14c439ea35a0020394
Sergii Leshchenko <sleshche@redhat.com>
2020-12-31 13:08:22 +0200

    Refactor a bit api-v200 tests

--- test/go/src/tests/api-v200_test.go
+++ test/go/src/tests/api-v200_test.go
@@ -24,7 +24,7 @@ type TestsToRun struct {
 	Tests []TestToRun `json:"Tests"`
 }
 
-// Structre for an individual test
+// Structure for an individual test
 type TestToRun struct {
 	FileName      string   `json:"FileName"`
 	ExpectOutcome string   `json:"ExpectOutcome"`
@@ -35,224 +35,268 @@ type TestToRun struct {
 const testDir = "../../../"
 const schemaDir = "../../../../"
 const jsonDir = "./json/v200/"
+
 const tempRootDir = "./tmp/"
-const tempDir = "./tmp/v200/"
+const tempDir = tempRootDir + "v200/"
 
 func Test_API_200(t *testing.T) {
-
-	// Clear the temp directory if it exists
-	if _, err := os.Stat(tempRootDir); !os.IsNotExist(err) {
-		os.RemoveAll(tempRootDir)
+	err := prepareTmpDir()
+	if err != nil {
+		t.Fatalf("Can't prepare tmpDir '%s`.Cause: %v", jsonDir, err)
 	}
-	os.Mkdir(tempRootDir, 0755)
-	os.Mkdir(tempDir, 0755)
 
-	// Read the content of the jso directory to find test files
+	// Read the content of the json directory to find test files
 	files, err := ioutil.ReadDir(jsonDir)
 	if err != nil {
 		t.Fatalf("Error finding test json files in : %s :  %v", jsonDir, err)
 	}
+	var testFiles []os.FileInfo
+	for _, f := range files {
+		// if the file begins with test- and ends .json it can be processed
+		if strings.HasPrefix(f.Name(), "test-") && strings.HasSuffix(f.Name(), ".json") {
+			testFiles = append(testFiles, f)
+		}
+	}
 	combinedTests := 0
 	combinedPasses := 0
-	for _, testJsonFile := range files {
+	combinedSkipped := 0
+	for _, testJsonFile := range testFiles {
+		testJsonContent, err := readTestJson(testJsonFile)
+		if err != nil {
+			t.Errorf("  FAIL : Failed to read and parse %s. Cause: %s", testJsonFile.Name(), err)
+			continue
+		}
 
-		// if the file begins with test- and ends .json it can be processed
-		if strings.HasPrefix(testJsonFile.Name(), "test-") && strings.HasSuffix(testJsonFile.Name(), ".json") {
+		t.Logf("INFO : File %s : SchemaFile : %s , SchemaVersion : %s", testJsonFile.Name(), testJsonContent.SchemaFile, testJsonContent.SchemaVersion)
 
-			// Open the json file which defines the tests to run
-			testJson, err := os.Open(filepath.Join(jsonDir, testJsonFile.Name()))
+		// Prepare the schema file
+		compiler := jsonschema.NewCompiler()
+		compiler.Draft = jsonschema.Draft7
+		schema, err := compiler.Compile(filepath.Join(schemaDir, testJsonContent.SchemaFile))
+		if err != nil {
+			t.Fatalf("  FAIL : Schema compile failed : %s: %v", testJsonContent.SchemaFile, err)
+		}
+
+		// create the temp directory to hold the generated yaml files
+		testTempDir := tempDir
+		testTempDir += strings.Split(testJsonFile.Name(), ".")[0]
+		os.Mkdir(testTempDir, 0755)
+
+		totalTests := 0
+		passTests := 0
+		skippedTests := 0
+
+		// for each of the test files specified
+		for _, testJsonFile := range testJsonContent.Tests {
+			testsToRunContent, err := readTestsToRun(testJsonFile)
 			if err != nil {
-				t.Errorf("  FAIL : Failed to open %s : %s", testJsonFile.Name(), err)
+				t.Errorf("Failed to parse test file '%s'. Cause: %v", testJsonFile, err)
 				continue
 			}
 
-			// Read contents of the json file which defines the tests to run
-			byteValue, err := ioutil.ReadAll(testJson)
-			if err != nil {
-				t.Errorf("FAIL : failed to read : %s : %v", testJsonFile.Name(), err)
-			}
-
-			var testJsonContent TestJson
-
-			// Unmarshall the contents of the json file which defines the tests to run for each test
-			err = json.Unmarshal(byteValue, &testJsonContent)
-			if err != nil {
-				t.Fatalf("FAIL : failed to unmarshal : %s : %v", testJsonFile.Name(), err)
-				continue
-			}
-
-			testJson.Close()
-
-			t.Logf("INFO : File %s : SchemaFile : %s , SchemaVersion : %s", testJsonFile.Name(), testJsonContent.SchemaFile, testJsonContent.SchemaVersion)
-
-			// Prepare the schema file
-			compiler := jsonschema.NewCompiler()
-			compiler.Draft = jsonschema.Draft7
-			schema, err := compiler.Compile(filepath.Join(schemaDir, testJsonContent.SchemaFile))
-			if err != nil {
-				t.Fatalf("  FAIL : Schema compile failed : %s: %v", testJsonContent.SchemaFile, err)
-			}
-
-			// create the tempdirectoy to hold the generated yaml files
-			testTempDir := tempDir
-			testTempDir += strings.Split(testJsonFile.Name(), ".")[0]
-			os.Mkdir(testTempDir, 0755)
-
-			passTests := 0
-			totalTests := 0
-
-			// for each of the test files specified
-			for m := 0; m < len(testJsonContent.Tests); m++ {
-
-				// Open the json file which defines the tests to run
-				testsToRunJson, err := os.Open(filepath.Join(jsonDir, testJsonContent.Tests[m]))
-				if err != nil {
-					t.Errorf("Failed to open tests %s : %s", testJsonContent.Tests[m], err)
+			// For each test defined in the test file
+			for _, testToRun := range testsToRunContent.Tests {
+				totalTests++
+				if testToRun.Disabled {
+					skippedTests++
 					continue
 				}
 
-				// Read contents of the json file which defines the tests to run
-				byteValue, err := ioutil.ReadAll(testsToRunJson)
+				// Open the file to contains the generated test yaml
+				f, err := os.OpenFile(filepath.Join(testTempDir, testToRun.FileName), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 				if err != nil {
-					t.Fatalf("FAIL : failed to read : %s : %v", testJsonContent.Tests[m], err)
+					t.Errorf("FAIL : Failed to open %s : %v", filepath.Join(testTempDir, testToRun.FileName), err)
+					continue
 				}
 
-				var testsToRunContent TestsToRun
-
-				// Unmarshall the contents of the json file which defines the tests to run for each test
-				err = json.Unmarshal(byteValue, &testsToRunContent)
-				if err != nil {
-					t.Fatalf("FAIL : failed to unmarshal : %s : %v", testJsonContent.Tests[m], err)
+				// If test requires a schema write it to the yaml file
+				if testJsonContent.SchemaVersion != "" {
+					f.WriteString("schemaVersion: " + testJsonContent.SchemaVersion + "\n")
 				}
 
-				testsToRunJson.Close()
-
-				// For each test defined in the test file
-				for i := 0; i < len(testsToRunContent.Tests); i++ {
-
-					if !testsToRunContent.Tests[i].Disabled {
-
-						totalTests++
-
-						// Open the file to containe the generated test yaml
-						f, err := os.OpenFile(filepath.Join(testTempDir, testsToRunContent.Tests[i].FileName), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-						if err != nil {
-							t.Errorf("FAIL : Failed to open %s : %v", filepath.Join(testTempDir, testsToRunContent.Tests[i].FileName), err)
-							continue
-						}
-
-						// If test requires a schema write it to the yaml file
-						if testJsonContent.SchemaVersion != "" {
-							f.WriteString("schemaVersion: " + testJsonContent.SchemaVersion + "\n")
-						}
-
-						testYamlComplete := true
-						// Now add each of the yaml sippets used the make the yaml file for test
-						for j := 0; j < len(testsToRunContent.Tests[i].Files); j++ {
-							// Read the snippet
-							data, err := ioutil.ReadFile(filepath.Join(testDir, testsToRunContent.Tests[i].Files[j]))
-							if err != nil {
-								t.Errorf("FAIL: failed reading %s: %v", filepath.Join(testDir, testsToRunContent.Tests[i].Files[j]), err)
-								testYamlComplete = false
-								continue
-							}
-							if j > 0 {
-								// Ensure appropriate line breaks
-								f.WriteString("\n")
-							}
-
-							// Add snippet to yaml file
-							f.Write(data)
-						}
-
-						if !testYamlComplete {
-							f.Close()
-							continue
-						}
-
-						// Read the created yaml file, ready for converison to json
-						data, err := ioutil.ReadFile(filepath.Join(testTempDir, testsToRunContent.Tests[i].FileName))
-						if err != nil {
-							t.Errorf("  FAIL: unable to read %s: %v", testsToRunContent.Tests[i].FileName, err)
-							f.Close()
-							continue
-						}
-
-						f.Close()
-
-						// Convert the yaml file to json
-						yamldoc, err := yaml.YAMLToJSON(data)
-						if err != nil {
-							t.Errorf("  FAIL : %s : failed to convert to json : %v", testsToRunContent.Tests[i].FileName, err)
-							continue
-						}
-
-						// validate the test yaml against the schema
-						if err = schema.Validate(bytes.NewReader(yamldoc)); err != nil {
-							if testsToRunContent.Tests[i].ExpectOutcome == "PASS" {
-								t.Errorf("  FAIL : %s : %s : Validate failure : %s", testsToRunContent.Tests[i].FileName, testJsonContent.SchemaFile, err)
-							} else if testsToRunContent.Tests[i].ExpectOutcome == "" {
-								t.Errorf("  FAIL : %s : No expected ouctome was set : %s  got : %s", testsToRunContent.Tests[i].FileName, testsToRunContent.Tests[i].ExpectOutcome, err.Error())
-							} else if !strings.Contains(err.Error(), testsToRunContent.Tests[i].ExpectOutcome) {
-								t.Errorf("  FAIL : %s : %s : Did not fail as expected : %s  got : %s", testsToRunContent.Tests[i].FileName, testJsonContent.SchemaFile, testsToRunContent.Tests[i].ExpectOutcome, err.Error())
-							} else {
-								passTests++
-								t.Logf("PASS : %s : %s: %s", testsToRunContent.Tests[i].FileName, testJsonContent.SchemaFile, testsToRunContent.Tests[i].ExpectOutcome)
-							}
-						} else if testsToRunContent.Tests[i].ExpectOutcome == "" {
-							t.Errorf("  FAIL : %s : devfile was valid - No expected ouctome was set.", testsToRunContent.Tests[i].FileName)
-						} else if testsToRunContent.Tests[i].ExpectOutcome != "PASS" {
-							t.Errorf("  FAIL : %s : devfile was valid - Expected Error not found :  %s", testsToRunContent.Tests[i].FileName, testsToRunContent.Tests[i].ExpectOutcome)
-						} else {
-							passTests++
-							t.Logf("PASS : %s : %s", testsToRunContent.Tests[i].FileName, testJsonContent.SchemaFile)
-						}
-						f.Close()
+				testYamlComplete := true
+				// Now add each of the yaml snippets used the make the yaml file for test
+				for j := 0; j < len(testToRun.Files); j++ {
+					// Read the snippet
+					data, err := ioutil.ReadFile(filepath.Join(testDir, testToRun.Files[j]))
+					if err != nil {
+						t.Errorf("FAIL: failed reading %s: %v", filepath.Join(testDir, testToRun.Files[j]), err)
+						testYamlComplete = false
+						continue
 					}
-				}
-			}
-			t.Logf("%s : %d of %d tests passed", testJsonFile.Name(), passTests, totalTests)
-			t.Logf("")
-			combinedTests += totalTests
-			combinedPasses += passTests
+					if j > 0 {
+						// Ensure appropriate line breaks
+						f.WriteString("\n")
+					}
 
+					// Add snippet to yaml file
+					f.Write(data)
+				}
+
+				if !testYamlComplete {
+					f.Close()
+					continue
+				}
+
+				// Read the created yaml file, ready for converison to json
+				data, err := ioutil.ReadFile(filepath.Join(testTempDir, testToRun.FileName))
+				if err != nil {
+					t.Errorf("  FAIL: unable to read %s: %v", testToRun.FileName, err)
+					f.Close()
+					continue
+				}
+
+				f.Close()
+
+				// Convert the yaml file to json
+				yamldoc, err := yaml.YAMLToJSON(data)
+				if err != nil {
+					t.Errorf("  FAIL : %s : failed to convert to json : %v", testToRun.FileName, err)
+					continue
+				}
+
+				// validate the test yaml against the schema
+				if err = schema.Validate(bytes.NewReader(yamldoc)); err != nil {
+					if testToRun.ExpectOutcome == "PASS" {
+						t.Errorf("  FAIL : %s : %s : Validate failure : %s", testToRun.FileName, testJsonContent.SchemaFile, err)
+					} else if testToRun.ExpectOutcome == "" {
+						t.Errorf("  FAIL : %s : No expected ouctome was set : %s  got : %s", testToRun.FileName, testToRun.ExpectOutcome, err.Error())
+					} else if !strings.Contains(err.Error(), testToRun.ExpectOutcome) {
+						t.Errorf("  FAIL : %s : %s : Did not fail as expected : %s  got : %s", testToRun.FileName, testJsonContent.SchemaFile, testToRun.ExpectOutcome, err.Error())
+					} else {
+						passTests++
+						t.Logf("PASS : %s : %s: %s", testToRun.FileName, testJsonContent.SchemaFile, testToRun.ExpectOutcome)
+					}
+				} else if testToRun.ExpectOutcome == "" {
+					t.Errorf("  FAIL : %s : devfile was valid - No expected ouctome was set.", testToRun.FileName)
+				} else if testToRun.ExpectOutcome != "PASS" {
+					t.Errorf("  FAIL : %s : devfile was valid - Expected Error not found :  %s", testToRun.FileName, testToRun.ExpectOutcome)
+				} else {
+					passTests++
+					t.Logf("PASS : %s : %s", testToRun.FileName, testJsonContent.SchemaFile)
+				}
+				f.Close()
+			}
+		}
+		t.Logf("PASS %s : \n"+
+			" %d tests;\n"+
+			" %d tests passed\n"+
+			" %d tests skipped.", testJsonFile.Name(), combinedPasses, combinedTests, skippedTests)
+		combinedTests += totalTests
+		combinedPasses += passTests
+		combinedSkipped += skippedTests
+
+	}
+
+	if combinedTests != combinedPasses+combinedSkipped {
+		t.Errorf("\nOVERALL FAIL : %d of %d tests failed.", combinedTests-combinedPasses-combinedSkipped, combinedTests)
+	} else {
+		t.Logf("\n--------------------\n"+
+			"OVERALL PASS: \n"+
+			" %d tests\n"+
+			" %d tests passed\n"+
+			" %d tests skipped.", combinedTests, combinedPasses, combinedSkipped)
+	}
+}
+
+func readTestsToRun(testPath string) (*TestsToRun, error) {
+	// Open the json file which defines the tests to run
+	testsToRunJson, err := os.Open(filepath.Join(jsonDir, testPath))
+	if err != nil {
+		return nil, err
+	}
+
+	// Read contents of the json file which defines the tests to run
+	byteValue, err := ioutil.ReadAll(testsToRunJson)
+	if err != nil {
+		return nil, err
+	}
+	testsToRunJson.Close()
+
+	testsToRunContent := &TestsToRun{}
+
+	// Unmarshall the contents of the json file which defines the tests to run for each test
+	err = json.Unmarshal(byteValue, testsToRunContent)
+	if err != nil {
+		return nil, err
+	}
+
+	return testsToRunContent, nil
+}
+
+func readTestJson(testJsonFile os.FileInfo) (*TestJson, error) {
+	// Open the json file which defines the tests to run
+	testJson, err := os.Open(filepath.Join(jsonDir, testJsonFile.Name()))
+	if err != nil {
+		return nil, err
+	}
+
+	// Read contents of the json file which defines the tests to run
+	byteValue, err := ioutil.ReadAll(testJson)
+	if err != nil {
+		return nil, err
+	}
+
+	testJsonContent := &TestJson{}
+	// Unmarshall the contents of the json file which defines the tests to run for each test
+	err = json.Unmarshal(byteValue, testJsonContent)
+	if err != nil {
+		return nil, err
+	}
+
+	testJson.Close()
+
+	return testJsonContent, nil
+}
+
+func prepareTmpDir() (err error) {
+	// Clear the temp directory if it exists
+	if _, err = os.Stat(tempRootDir); err == nil {
+		err := os.RemoveAll(tempRootDir)
+		if err != nil {
+			return err
 		}
 	}
-
-	if combinedTests != combinedPasses {
-		t.Errorf("OVERALL FAIL : %d of %d tests failed.", (combinedTests - combinedPasses), combinedTests)
-	} else {
-		t.Logf("OVERALL PASS : %d of %d tests passed.", combinedPasses, combinedTests)
+	if err != nil && !os.IsNotExist(err) {
+		return err
 	}
 
+	err = os.Mkdir(tempRootDir, 0755)
+	if err != nil {
+		return err
+	}
+
+	err = os.Mkdir(tempDir, 0755)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 // Users struct which contains
 // an array of users
 type Command struct {
-	id            string `yaml:"id"`
+	ID            string `yaml:"ID"`
 	SchemaVersion string `yam:"SchemaVersion"`
 }
 
 func Test_WriteYaml(t *testing.T) {
-
 	command := Command{}
 
-	command.id = "TestYaml"
+	command.ID = "TestYaml"
 	command.SchemaVersion = "2.0.0"
 
 	c, err := yaml.Marshal(&command)
 
 	f, err := os.Create("/tmp/data")
+
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer f.Close()
 
 	err = ioutil.WriteFile("command.yaml", c, 0644)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	f.Close()
-
 }
